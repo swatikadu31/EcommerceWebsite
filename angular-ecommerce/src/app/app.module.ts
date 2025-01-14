c43c170b7e05a6e059578ed3c17afe7b459c0922
Swati Kadu <swatikadu31@gmail.com>
2025-01-14 11:53:49 +0530

    Initial commit

---
+++ angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule} from '@angular/common/http'
+import { ProductService } from './services/product.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,HttpClientModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay()),ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
