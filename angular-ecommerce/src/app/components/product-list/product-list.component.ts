f535486113cec30bb620ee51a34fc067c769f17c
Swati Kadu <swatikadu31@gmail.com>
2025-01-16 16:52:05 +0530

    Search for product-Dynamic Search Component

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -14,7 +14,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductListComponent implements OnInit{
   products:Product[]=[];
   currentCategoryId!:number;
-
+  currentCategoryName: string = "";
 constructor(private productService:ProductService,
   private route:ActivatedRoute){
 
@@ -30,12 +30,16 @@ listProducts() {
   // check if "id"parameter is available
   const hasCategoryId:boolean= this.route.snapshot.paramMap.has('id');
 if(hasCategoryId){
-  //get the "id" param string. convert string to a number using the "+" symbol 
-  this.currentCategoryId=+this.route.snapshot.paramMap.get('id')!;
+   // get the "id" param string. convert string to a number using the "+" symbol
+   this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+     
+   // get the "name" param string
+   this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
 }
 else{
   //not category id available---- default to current category id is 1
-  this.currentCategoryId=1;
+  this.currentCategoryId = 1;
+  this.currentCategoryName = 'Books';
 }
 
   this.productService.getProductList(this.currentCategoryId).subscribe({
