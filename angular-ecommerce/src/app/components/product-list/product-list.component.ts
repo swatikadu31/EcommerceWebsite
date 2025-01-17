08182b1e1e3ce5ee79a029768d9787fe9dd9148e
Swati Kadu <swatikadu31@gmail.com>
2025-01-17 17:07:35 +0530

    Search by keyword

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -15,6 +15,9 @@ export class ProductListComponent implements OnInit{
   products:Product[]=[];
   currentCategoryId!:number;
   currentCategoryName: string = "";
+  searchMode:boolean=false;
+
+
 constructor(private productService:ProductService,
   private route:ActivatedRoute){
 
@@ -27,6 +30,31 @@ ngOnInit(): void {
 
 }
 listProducts() {
+
+  this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+  if (this.searchMode) {
+    this.handleSearchProducts();
+  }
+  else {
+    this.handleListProducts();
+  }
+
+}
+
+handleSearchProducts() {
+
+  const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+  // now search for the products using keyword
+  this.productService.searchProducts(theKeyword).subscribe(
+    data => {
+      this.products = data;
+    }
+  )
+}
+
+handleListProducts(){
   // check if "id"parameter is available
   const hasCategoryId:boolean= this.route.snapshot.paramMap.has('id');
 if(hasCategoryId){
--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -8,8 +8,6 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  
-
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
@@ -21,9 +19,19 @@ export class ProductService {
     // need to build URL based on category id 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+
+    // need to build URL based on the keyword 
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(map(response => response._embedded.products));
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
