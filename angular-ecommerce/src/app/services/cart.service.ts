063f7b460525b5f8052e65433f1cd2f02c2944cf
Swati Kadu <swatikadu31@gmail.com>
2025-02-27 14:25:10 +0530

    handling browser refresh-sessionStorae/localStorage

--- angular-ecommerce/src/app/services/cart.service.ts
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -11,7 +11,21 @@ export class CartService {
   totalPrice: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   totalQuantity: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  constructor() { 
+    //read the data from storage
+
+    let data = JSON.parse(this.storage.getItem('cartItems') || '[]');
+    if(data !=null){
+      this.cartItems=data;
+      //compute total based on the data that is read from storage
+
+      this.computeCartTotals();
+    }
+  }
+
+  //storage:Storage = sessionStorage;
+  storage:Storage = localStorage;
+
 
   addToCart(theCartItem: CartItem) {
 
@@ -62,6 +76,13 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    //persist cart data
+    this.persistCartItems();
+  }
+
+  persistCartItems(){
+    this.storage.setItem('cartItems',JSON.stringify(this.cartItems));
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
