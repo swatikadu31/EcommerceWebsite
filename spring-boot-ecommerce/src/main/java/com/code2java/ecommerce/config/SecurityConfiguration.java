6ff4869d2ac7a063af6210d2c64c6719eed5b90d
Swati Kadu <swatikadu31@gmail.com>
2025-03-06 12:24:22 +0530

    secure backend-unexpected result(SecurityConfiguration)

---
+++ spring-boot-ecommerce/src/main/java/com/code2java/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.code2java.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import com.okta.spring.boot.oauth.Okta;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	 http
+         .authorizeHttpRequests(auth -> auth
+                 .requestMatchers("/api/orders/**").authenticated() // Secure orders
+                 .anyRequest().permitAll()
+         )
+         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) // Enforce JWT authentication
+         .cors(Customizer.withDefaults()) // Enable CORS
+         .csrf(AbstractHttpConfigurer::disable); // Disable CSRF (for APIs)
+
+     // Set content negotiation strategy
+     http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+     // Return a 401 Unauthorized response if authentication fails
+     Okta.configureResourceServer401ResponseBody(http);
+
+     return http.build();
+
+        
+    }
+    }
--- spring-boot-ecommerce/src/main/java/com/code2java/ecommerce/dao/OrderRepository.java
+++ spring-boot-ecommerce/src/main/java/com/code2java/ecommerce/dao/OrderRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.code2java.ecommerce.entity.Order;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "orders")
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	
 	Page<Order> findByCustomerEmailOrderByDateCreateDesc(@Param("email") String email, Pageable page);
