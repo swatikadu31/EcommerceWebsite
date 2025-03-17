dc698f596ced0b7e97c1384e8906e05a8a4a9789
Swati Kadu <swatikadu31@gmail.com>
2025-03-17 12:06:34 +0530

    secure communication with HTTPS

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
 import { ProductService } from './services/product.service';
 import { Routes,RouterModule, Router } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
@@ -23,6 +23,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 const oktaConfig=myAppConfig.oidc;
 
@@ -84,7 +85,8 @@ const routes: Routes = [
   ],
   providers: [
     provideClientHydration(withEventReplay()),ProductService,
-    {provide:OKTA_CONFIG,useValue:{oktaAuth}}
+    {provide:OKTA_CONFIG,useValue:{oktaAuth}},
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true}
   ],
   bootstrap: [AppComponent]
 })
