01d6b38d3ecb242fb94a02ac767d9c9963b513fa
Swati Kadu <swatikadu31@gmail.com>
2025-03-17 13:46:15 +0530

    change port No for QA environment -angular

--- spring-boot-ecommerce/src/main/java/com/code2java/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/code2java/ecommerce/dao/ProductRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-//@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("https://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
